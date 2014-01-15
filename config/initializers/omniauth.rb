fada67fb3ab9668f98e55183fff5bf1492963acd
pobing <cn.jdong@gmail.com>
2014-01-16 00:01:46 +0800

    update weibo,qq,oauth key

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,10 @@
 # https://github.com/intridea/omniauth
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
-  provider :weibo, '1978116365', '82fa3e1654c905b5b545a16945a0873d'
-  provider :qq, '100317654', 'b1700db0db66d18f0cd507a0068b2f82'
+  # provider :weibo, '1978116365', '82fa3e1654c905b5b545a16945a0873d'
+  # provider :qq, '100317654', 'b1700db0db66d18f0cd507a0068b2f82'
+  provider :weibo, '1603641205','6c6830b69f73e5e10902ab8c5a911f70'
+  provider :qq, '101008038','dec9253d5cdb2c63868d2bd6e95cdfa5'
 end
 
 
