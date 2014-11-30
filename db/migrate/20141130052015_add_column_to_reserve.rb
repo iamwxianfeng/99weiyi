92500353d4e8337a641957942873f11e7109353b
jidongdong <jidongdong@admaster.com.cn>
2014-11-30 20:46:08 +0800

    update migrate

--- db/migrate/20141130052015_add_column_to_reserve.rb
+++ db/migrate/20141130052015_add_column_to_reserve.rb
@@ -4,6 +4,6 @@ class AddColumnToReserve < ActiveRecord::Migration
     add_column :reserves, :weight_id, :integer, default: 0
     add_column :reserves, :desc, :string
     add_column :reserves, :tailor_id, :integer
-    remove_column :reserves, :store_id, :integer
+    # remove_column :reserves, :store_id, :integer
   end
 end
