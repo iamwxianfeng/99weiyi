0c2c5d8a84fe21625ab4918c6d0a28ec8d276e16
jidongdong <jidongdong@admaster.com.cn>
2014-12-12 09:52:52 +0800

    rm db/migrate/20141211003341_change_reserve_status_type.rb

--- db/migrate/20141211003341_change_reserve_status_type.rb
+++
@@ -1,9 +0,0 @@
-class ChangeReserveStatusType < ActiveRecord::Migration
-  def up
-    change_column :reserves , :status , :string, limit: 15, default: 'waiting'
-  end
-
-  def down
-    change_column :reserves , :status , :integer, default: 0
-  end
-end
